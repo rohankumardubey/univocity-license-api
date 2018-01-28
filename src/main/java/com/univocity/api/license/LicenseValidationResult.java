4609c2b5cc4ff369c3c46c3c52cd7f4922b43884
Jeronimo Backes <jbax@univocity.com>
2018-01-28 12:10:30 +1030

    adding a human readable description to LicenseValidationResult enumeration.

--- src/main/java/com/univocity/api/license/LicenseValidationResult.java
+++ src/main/java/com/univocity/api/license/LicenseValidationResult.java
@@ -20,70 +20,93 @@ public enum LicenseValidationResult {
 	/**
 	 * The license registration details are incomplete.
 	 */
-	INCOMPLETE(1),
+	INCOMPLETE(1, "Incomplete"),
 	/**
 	 * The license has expired, i.e. {@link License#getLicenseExpirationDate()} is not {@code null} and the current date
 	 * is after the expiration date.
 	 */
-	EXPIRED(2),
+	EXPIRED(2, "Expired"),
 	/**
 	 * The current version of the software being used is not supported by the current license, i.e.
 	 * {@link License#getSupportEndDate()} is not {@code null} and {@link ProductVersion#releaseDate()} is after the
 	 * support end date.
 	 */
-	SUPPORT_ENDED(3),
+	SUPPORT_ENDED(3, "Support ended"),
 
 	/**
 	 * Indicates the current hardware doesn't match the original hardware signature from when the license was generated.
 	 * The license can be reassigned to the current hardware using {@link LicenseManager#assignLicense(File)}
 	 * or {@link LicenseManager#assignLicense(String, String)}
 	 */
-	UNKNOWN_HOST(4),
+	UNKNOWN_HOST(4, "Unknown host"),
 
 	/**
 	 * Indicates that the current license is a trial and the trial period has ended
 	 */
-	TRIAL_EXPIRED(5),
+	TRIAL_EXPIRED(5, "Expired"),
 
 	/**
 	 * Indicates the user tried to register for a trial license for a second time. This error is only returned by the
 	 * remote server.
 	 */
-	RETRIAL_ATTEMPTED(6),
+	RETRIAL_ATTEMPTED(6, "Retrial attempted"),
 
 	/**
 	 * There is no license present locally. Note that license validation process will update the local license after
 	 * every call to {@link LicenseManager#validate(LicenseValidationAction)}. If the license got reassigned to someone
 	 * else the local license will be deleted.
 	 */
-	NOT_FOUND(7),
+	NOT_FOUND(7, "Not found"),
 
 	/**
 	 * Indicates the license is invalid, i.e. product or registration information doesn't match, or license has been
 	 * tampered with.
 	 */
-	INVALID(8),
+	INVALID(8, "Invalid"),
 
 	/**
 	 * Returned by the remote server to indicate that trial licenses have been disabled, or that a purchased license has
 	 * been reassigned to someone else. The local license will be removed automatically and further calls to
 	 * {@link LicenseManager#validate(LicenseValidationAction)} should produce {@link #NOT_FOUND}
 	 */
-	DISABLED(9),
+	DISABLED(9, "Disabled"),
 
 	/**
 	 * The license is valid
 	 */
-	VALID(10),
+	VALID(10, "Valid"),
 
 	/**
 	 * An internal error occurred while validating the license. Does not mean that the license is invalid.
 	 */
-	ERROR(0);
+	ERROR(0, "Error");
 
 	public final int code;
+	public final String description;
 
-	LicenseValidationResult(int code){
+	private static final LicenseValidationResult[] CODE_MAP;
+
+	static {
+		int length = LicenseValidationResult.values().length;
+		CODE_MAP = new LicenseValidationResult[length];
+		for (LicenseValidationResult t : LicenseValidationResult.values()) {
+			CODE_MAP[t.code] = t;
+		}
+	}
+
+	LicenseValidationResult(int code, String description) {
 		this.code = code;
+		this.description = description;
+	}
+
+	/**
+	 * Returns the {@code LicenseValidationResult} associated with a given code.
+	 *
+	 * @param code the code of a {@code LicenseValidationResult}
+	 *
+	 * @return the value associated with the code.
+	 */
+	public static LicenseValidationResult fromCode(int code) {
+		return CODE_MAP[code];
 	}
 }
